7cb02ddc4002fe677edf377f02aa5f246a9c2f9a
Jackson Jaensch <jackson.jaensch14@gmail.com>
2026-02-12 15:49:38 +1030

    Accessibility colour changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,11 @@ module.exports = {
     extend: {
       colors: {
         'deep-teal': '#006B7D',
+        'dark-teal': '#005161',
+        'navbar-teal': '#0f6375',
         'slate': '#3E4A5B',
         'warm-sand': '#D4A574',
-        'sunset-orange': '#C85A00',
+        'sunset-orange': '#FFA500',
         'off-white': '#F7F5F2',
       },
       fontFamily: {
